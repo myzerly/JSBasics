226de4d40acdb475d44e0e9b9a79553f086a8ba5
nemesisjoe <nic.edwrds@gmail.com>
2015-04-25 12:30:21 -0700

    uses basic JS to update header styles

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,15 +1,15 @@
 var formattedName = HTMLheaderName.replace("%data%", "Nic Edwards"); 
-var formattedRole = HTMLheaderRole.replace("%data%" ,"Product Manager")
+var formattedRole = HTMLheaderRole.replace("%data%" ,"Fun Dude")
 
 
-var awesomeThoughts = "I am Nic and he is totally awesome"
-var funThoughts = awesomeThoughts.replace("awesome", "fun");
-
-console.log(awesomeThoughts);
-console.log(funThoughts);
+$("#header").prepend(formattedRole);
+$("#header").append(formattedName);
 
 
-$("#main").prepend(formattedName);
-$("#main").append(formattedRole);
 // $("#main").append(funThoughts);
 
+// var awesomeThoughts = "I am Nic and he is totally awesome"
+// var funThoughts = awesomeThoughts.replace("awesome", "fun");
+
+// console.log(awesomeThoughts);
+// console.log(funThoughts);
